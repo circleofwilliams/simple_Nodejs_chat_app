20db9fe41828900266415c5445008a3a1c5c1e7e
Olamide Williams <circleofwilliams@gmail.com>
2024-01-15 11:19:14 +0100

    feat: each client now has a username that is asked for the first time the clients connects, all users are also notified when a new client joins the servr after the client has set the username

--- chat_app/server.js
+++ chat_app/server.js
@@ -2,20 +2,51 @@ const net = require("net");
 
 const port = 3000;
 const hostingInterface = "127.0.0.1";
-let clients = [];
+let sockets = [];
+let clientsUsername = [];
 
 const server = net.createServer();
 
 server.on("connection", (socket) => {
+  let firstMessage = true;
+  let tag;
+
   console.log("A new connection to the server.");
-  clients.push(socket);
-  console.log("Number of users", clients.length);
+  socket.write("...Kindly enter your username...");
 
   socket.on("data", (data) => {
-    clients.map((c) => {
-      c.write(data);
-    });
-    // socket.write(data);
+    if (firstMessage) {
+      const userName = data.toString();
+      tag = userName;
+      clientsUsername.push(userName);
+      sockets.push(socket);
+      firstMessage = false;
+      sockets.map((s) => {
+        if (s == socket) socket.write("You joined the chat!");
+        else {
+          message = "Alert: " + tag + " joined the chat!";
+          s.write(message);
+        }
+      });
+      console.log(
+        "Number of users",
+        clientsUsername.length,
+        ": ",
+        clientsUsername
+      );
+    } else {
+      const socketIndex = sockets.indexOf(socket);
+      let sendersTag = clientsUsername[socketIndex];
+      sockets.map((s) => {
+        if (s == socket) {
+          message = "You: " + data.toString();
+          s.write(message);
+        } else {
+          message = sendersTag + ": " + data.toString();
+          s.write(message);
+        }
+      });
+    }
   });
 });
 
